14bdb6d7da834391d7fa577d28d0e63536e781fc
I. M. Napal <ignacionapal2400@gmail.com>
2026-09-10 17:06:30 -0300

    Completado para todos los métodos.

--- src/Controllers/AccountController.cs
+++ src/Controllers/AccountController.cs
@@ -4,6 +4,8 @@ using System.Buffers;
 using api_poo.Data;
 using api_poo.Entities;
 using api_poo.Models;
+using System.Net.WebSockets;
+using System.Reflection.Metadata.Ecma335;
 
 namespace api_poo.Controllers;
 
@@ -30,4 +32,32 @@ public class AccountController : ControllerBase
         
     }
 
+    [HttpGet]
+    public ActionResult<List<BankAccountDto>> Get()
+    {
+        var list = _bankAccountRepository.List();
+        List<BankAccountDto> res = new();
+        foreach (BankAccount acc in list)
+        {
+            res.Add(BankAccountDto.Create(acc));
+        }
+        return Ok(res);
+    }
+    public ActionResult<BankAccountDto> GetById([FromBody]int id)
+    {
+        BankAccount foundAccount = _bankAccountRepository.GetById(id);
+        return Ok(BankAccountDto.Create(foundAccount));
+    }
+    [HttpPatch]
+    public ActionResult<bool> Update([FromBody] BankAccount bankAccount)
+    {
+        _bankAccountRepository.Update(bankAccount);
+        return Ok(true);
+    }
+    [HttpDelete]
+    public ActionResult<bool> Delete([FromBody] BankAccount bankAccount)
+    {
+        _bankAccountRepository.Delete(bankAccount);
+        return Ok(true);
+    }
 }
--- src/Data/BankAccountRepository.cs
+++ src/Data/BankAccountRepository.cs
@@ -1,6 +1,7 @@
 using System.Reflection.Metadata;
 using api_poo.Interfaces;
 using api_poo.Entities;
+using System;
 
 namespace api_poo.Data;
 
@@ -10,12 +11,14 @@ public class BankAccountRepository : IBankAccountRepository
 
     public BankAccount GetById(int id)
     {
-        throw new Exception("Not implemented");
+        BankAccount returnedBA = _accounts.FirstOrDefault(ba => ba.Number == id.ToString()) ?? throw new KeyNotFoundException($"No se encontró una cuenta con el número {id}.");
+        return returnedBA;
     }
 
     public List<BankAccount> List()
     {
-        throw new Exception("Not implemented");
+        List<BankAccount> accountList = _accounts ?? throw new Exception("No hay cuentas registradas.");
+        return accountList;
     }
 
     public BankAccount Add(BankAccount entity)
@@ -26,12 +29,23 @@ public class BankAccountRepository : IBankAccountRepository
 
     public void Update(BankAccount entity)
     {
-        throw new Exception("Not implemented");
+        int accountIndex = _accounts.FindIndex(acc => acc.Number == entity.Number);
+        _accounts[accountIndex] = accountIndex == -1 
+            ? throw new KeyNotFoundException($"No se encontró una cuenta con el número {entity.Number}.") 
+            : entity;
     }
 
     public void Delete(BankAccount entity)
     {
-        throw new Exception("Not implemented");
+        int accountIndex = _accounts.FindIndex(acc => acc.Number == entity.Number);
+        if (accountIndex == -1)
+        {
+            throw new KeyNotFoundException($"No se encontró una cuenta con el número {entity.Number}.");
+        }
+        else
+        {
+            _accounts.RemoveAt(accountIndex);
+        }
     }
 
 }
